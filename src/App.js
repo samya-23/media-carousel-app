e9a26c91d45623ec696ae450079aa0b720fdc191
samya-23 <you.are.my.sunshine7648@gmail.com>
2025-06-10 14:40:20 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
+import MediaCarousel from './components/MediaCarousel';
 
 function App() {
+  const mediaItems = [
+    {
+      type: 'image',
+      src: 'https://media.licdn.com/dms/image/v2/D5612AQGfkrqIN_4sjA/article-cover_image-shrink_720_1280/B56ZXxhnAZGcAI-/0/1743513879404?e=2147483647&v=beta&t=-eKhUj8F0d4PVgUe16JoGNq4eSPdcgkpPSJJY1UtTXU'
+    },
+    {
+      type: 'video',
+      src: 'https://www.youtube.com/embed/FwOTs4UxQS4?si=F1-RYbHgKnE_cBgV'
+    },
+    {
+      type: 'image',
+      src: 'https://www.intelliscence.com/wp-content/uploads/2024/07/Custom-Software-Development.jpg'
+    },
+    {
+      type: 'video',
+      src: 'https://www.youtube.com/embed/Cg2A3L58pY0?si=ePEpmx5Nx52HsQk3'
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hero">
+        <h1>Experience <span>Codepackers</span></h1>
+        <p>Innovative AI-first solutions & platform showcase</p>
+      </div>
+      <MediaCarousel mediaItems={mediaItems} />
     </div>
   );
 }
---
+++ src/components/MediaCarousel.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './MediaCarousel.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const MediaCarousel = ({ mediaItems }) => {
+  return (
+    <div className="media-carousel">
+      <Swiper
+        modules={[Pagination]}
+        pagination={{ clickable: true }}
+        spaceBetween={30}
+        slidesPerView={1}
+        className="swiper-wrapper-custom"
+      >
+        {mediaItems.map((item, index) => (
+          <SwiperSlide key={index}>
+            {item.type === 'image' ? (
+              <img src={item.src} alt={`Slide ${index}`} />
+            ) : (
+              <iframe
+                src={item.src}
+                title={`Video ${index}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            )}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default MediaCarousel;
